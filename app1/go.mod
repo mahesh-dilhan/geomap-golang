3f195b14f3ba30e96ae79520def3fe6f1f3908e5
mahesh <mail.mahesh.lk@gmail.com>
2021-07-11 14:42:38 +0800

    go client gomap

---
+++ app1/go.mod
@@ -0,0 +1,8 @@
+module githb.com/mahesh-dilhan/geo-golang
+
+go 1.16
+
+require (
+	github.com/kr/pretty v0.2.1 // indirect
+	googlemaps.github.io/maps v1.3.2 // indirect
+)
---
+++ app1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/kr/pretty"
+	"googlemaps.github.io/maps"
+)
+
+func main() {
+	c, err := maps.NewClient(maps.WithAPIKey("Insert-API-Key-Here"))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	r := &maps.DirectionsRequest{
+		Origin:      "Sydney",
+		Destination: "Perth",
+	}
+	route, _, err := c.Directions(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	pretty.Println(route)
+}
